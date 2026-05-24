d312f75d2a25fed337f104fcfc7c1beded9d5b22
jahnvi dubey <jahnvi.dubey10@gmail.com>
2026-05-24 14:53:26 +0530

    changes in  postgres

--- backend/database.py
+++ backend/database.py
@@ -1,23 +1,51 @@
+# # database.py
+
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+
+# # PostgreSQL Configuration
+# DB_USER = "postgres"
+# DB_PASSWORD = "Dubey123"
+# DB_HOST = "localhost"
+# DB_PORT = "5432"
+# DB_NAME = "scotlandyard"
+
+# SQLALCHEMY_DATABASE_URL = (
+#     f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# )
+
+# # Engine
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL,
+#     pool_pre_ping=True,
+#     echo=False
+# )
+
+# # Session
+# SessionLocal = sessionmaker(
+#     autocommit=False,
+#     autoflush=False,
+#     bind=engine
+# )
+
+# # Base
+# Base = declarative_base()
+
+
 # database.py
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-# PostgreSQL Configuration
-DB_USER = "postgres"
-DB_PASSWORD = "Dubey123"
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "scotlandyard"
-
-SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+# Render PostgreSQL URL
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Engine
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
+    DATABASE_URL,
     pool_pre_ping=True,
     echo=False
 )
